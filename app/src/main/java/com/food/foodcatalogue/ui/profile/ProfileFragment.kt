d39cc09e1664e0cdcaca47e9a7d441996f2b2207
maghfirah <maghfirahht@gmail.com>
2021-12-15 01:48:06 +0800

    Add profile

---
+++ app/src/main/java/com/food/foodcatalogue/ui/profile/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.kocaksapp.foodcatalogue.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kocaksapp.foodcatalogue.databinding.FragmentProfileBinding
+
+class ProfileFragment: Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding as FragmentProfileBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
